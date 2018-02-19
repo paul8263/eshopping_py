8d19c9793c6a4b139dc6cb66dc0e9b86ce246f27
paulzhang <paul8263@gmail.com>
2018-02-19 16:06:52 +0800

    added commodity model

--- commodities/admin.py
+++ commodities/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+
+class CommodityImageInline(admin.StackedInline):
+    model = models.CommodityImage
+
+
+@admin.register(models.Commodity)
+class CommodityAdmin(admin.ModelAdmin):
+    fields = ('name', 'description', 'price')
+    inlines = (CommodityImageInline,)
+
+
+
--- commodities/models.py
+++ commodities/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Commodity(models.Model):
+    name = models.CharField(blank=False, null=False, max_length=512)
+    description = models.CharField(blank=False, null=False, max_length=5000)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+
+
+class CommodityImage(models.Model):
+    commodity = models.ForeignKey('Commodity', on_delete=models.CASCADE)
+    image = models.ImageField()
